8e806ace24e15ac77baae19e7e59990ff6c87c58
Pedro <phenri0203@gmail.com>
2026-08-14 22:15:46 -0300

    funcao subtracao criada

--- main.js
+++ main.js
@@ -12,5 +12,16 @@ function somar() {
 botaoAdicao.onclick = somar
 
 const botaoSubtracao = document.getElementById('subtracao')
+
+function subtrair() {
+    const num1 = Number (document.getElementById('numero1').value)
+    const num2 = Number (document.getElementById('numero2').value)
+    const resultado = document.getElementById('resultado')
+
+    resultado.textContent = num1 - num2
+}
+
+botaoSubtracao.onclick = subtrair
+
 const botaoMultiplicacao = document.getElementById('multiplicacao')
 const botaoDivisao = document.getElementById('divisao')      
