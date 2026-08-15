2b39c50e372509766441e418efeee5440a3bfe54
Pedro <phenri0203@gmail.com>
2026-08-14 22:13:07 -0300

    criada funçao de somar

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict'
+const botaoAdicao = document.getElementById('adicao')
+
+function somar() {
+    const num1 = Number (document.getElementById('numero1').value)
+    const num2 = Number (document.getElementById('numero2').value)
+    const resultado = document.getElementById('resultado')
+
+    resultado.textContent = num1 + num2
+}
+
+botaoAdicao.onclick = somar
+
+const botaoSubtracao = document.getElementById('subtracao')
+const botaoMultiplicacao = document.getElementById('multiplicacao')
+const botaoDivisao = document.getElementById('divisao')      
